c385fa078e02de84940a0d26825b8ebf31bfe642
Mert Acel <mert.acel@ardictech.com>
2019-02-04 11:40:11 +0300

    change : content

--- rest/drom/postadddrom.go
+++ rest/drom/postadddrom.go
@@ -6,8 +6,8 @@ post
 https://api.ardich.com:443/api/v3/dromdeviceconfiguration/add
 
 {
-   "configurationId": "c71920c1-728a-49d0-88b8-d1eb678ceb17",
-  "deviceId": "868727040000183"
+   "configurationId": "xxxx",
+  "deviceId": "yyyy"
 }
 
 */
